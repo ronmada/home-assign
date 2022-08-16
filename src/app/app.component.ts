dcc429872f1ccdcd33a6adfe58f1ba8b6821a55d
Ron Wexler <ronwexler93@gmail.com>
2022-08-17 00:56:01 +0300

    done for the day. almsot finished first part

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'home-assign';
+  public isShowMore = false;
+
+  public toggleIsShowMore(): void {
+    this.isShowMore = !this.isShowMore;
+  }
 }
